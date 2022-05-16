05f1fb96e192e45103978ab3dfaffd0008e0e8bc
druva1997 <103628454+druva1997@users.noreply.github.com>
2022-05-16 10:47:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const bookData = [];
 
+let name1 = []
+let email1 = []
+let number1 = []
+
+const hname = document.querySelector(".hname")
+let hemail = document.querySelector(".hemail")
+let hnumber = document.querySelector(".hnumber")
+console.log(hname)
+
+let show = document.querySelector(".appt")
+
 
 function getAppointmentData(e ){
 e.preventDefault()
 const name = document.getElementById("name")
 const email = document.getElementById("email")
 const number = document.getElementById("number")
+
+name1.push(name.value)
+email1.push(email.value)
+number1.push(number.value)
+
+console.log(name1)
+console.log(email1)
+console.log(number1)
+
+
+
+
+
 
 const data = { name: name.value, email: email.value, number: number.value
 
@@ -15,9 +39,23 @@ bookData.push(data)
 
 console.log(bookData)
 }
-arrname = new Array();
-arremail = new Array();
-arrnumber = new Array();
 
+
+
+let temp = document.querySelector(".temp")
+let submit = document.querySelector(".submit")
+
+
+let showAp= document.querySelector (".showAppt")
+showAp.addEventListener("click",function(){
+    hname.innerText = name1[name1.length-1]
+    hemail.innerText = email1[email1.length-1]
+    hnumber.innerText = number1[number1.length-1]
+    console.log(hname.innerText)
+    console.log(hemail.innerText)
+    console.log(hnumber.innerText)
+    show.style.display="flex";
+    console.log(show)
+})
 
 
